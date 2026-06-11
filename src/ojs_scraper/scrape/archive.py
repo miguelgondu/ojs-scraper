1627226b7e27b3643e2fcaae1fa496f2eb679447
Juan R. Loaiza <juanrloaiza@gmail.com>
2026-06-10 20:51:55 -0400

    feat: adds some logging to ArchiveScraper

--- src/ojs_scraper/scrape/archive.py
+++ src/ojs_scraper/scrape/archive.py
@@ -33,6 +33,7 @@ class ArchiveScraper:
 
             if issue_pages.union(current_issue_links) > issue_pages:
                 issue_pages = issue_pages.union(current_issue_links)
+                logger.info("Visited and added %d links from %s", len(current_issue_links), current_url)
                 counter += 1
             else:
                 break
