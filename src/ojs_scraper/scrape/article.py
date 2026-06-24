58e64f9b25afcf27807d550889b8a30cda23f103
Miguel González Duque <miguelgondu@gmail.com>
2026-06-24 12:08:25 -0500

    feat: modifies the logic of scraper to use functions and re instead of classes

--- src/ojs_scraper/scrape/article.py
+++ src/ojs_scraper/scrape/article.py
@@ -1,83 +1,75 @@
+import re
 from datetime import datetime
 from typing import cast
 
-from bs4 import Tag
+from bs4 import BeautifulSoup, Tag
 
 from ojs_scraper.models.article import ArticleFormat, BaseArticle
 from ojs_scraper.utils.constants import PREFERENCE_ORDER
 from ojs_scraper.utils.soup import soupify
 
 
-class ArticleScraper:
-    def __init__(
-        self, article_url: str, article_galley_class: str | None = None
-    ) -> None:
-        self.article_url = article_url
-        self.article_soup = soupify(article_url)
+def _extract_metadata(article_soup: BeautifulSoup) -> dict[str, str]:
+    meta = article_soup.find_all("meta", attrs={"name": True})
 
-        self.article_galley_class = article_galley_class or "obj_galley_link"
+    metadata = {}
+    for item in meta:
+        metadata[item["name"]] = item["content"]  # type: ignore
 
-    def extract_metadata(self) -> dict[str, str]:
-        """
-        Extracts metadata from a BeautifulSoup object.
+    return metadata
 
-        Returns a tuple of the article ID and its metadata.
-        """
 
-        meta = self.article_soup.find_all("meta", attrs={"name": True})
+def _find_tag_with_best_format(link_tags: list[Tag]) -> tuple[Tag, ArticleFormat]:
+    article_tag = None
+    article_format = None
+    for format_ in PREFERENCE_ORDER:
+        candidate_tags = [
+            tag for tag in link_tags if format_ in tag.get_text(strip=True).lower()
+        ]
 
-        metadata = {}
-        for item in meta:
-            metadata[item["name"]] = item["content"]  # type: ignore
-
-        return metadata
-
-    def _find_tag_with_best_format(
-        self, link_tags: list[Tag]
-    ) -> tuple[Tag, ArticleFormat]:
-        article_tag = None
-        article_format = None
-        for format_ in PREFERENCE_ORDER:
-            candidate_tags = [
-                tag for tag in link_tags if format_ in tag.get_text(strip=True).lower()
-            ]
-
-            if len(candidate_tags) > 0:
-                # We found a link with the desired format.
-                # We can return the article object.
-                article_tag = candidate_tags[0]
-                article_format = format_
-                break
-        else:
-            raise ValueError(
-                f"None of the preferred formats {PREFERENCE_ORDER} found in article "
-                f"links. {[tag.get_text(strip=True) for tag in link_tags]}"
-            )
-
-        if "href" not in article_tag.attrs:
-            article_tag = article_tag.find("a")
-
-        return cast("Tag", article_tag), ArticleFormat(article_format.lower())
-
-    def scrape_article(self) -> BaseArticle:
-        raw_metadata = self.extract_metadata()
-
-        link_tags = cast(
-            "list[Tag]",
-            self.article_soup.find_all(attrs={"class": f"{self.article_galley_class}"}),
+        if len(candidate_tags) > 0:
+            # We found a link with the desired format.
+            # We can return the article object.
+            article_tag = candidate_tags[0]
+            article_format = format_
+            break
+    else:
+        raise ValueError(
+            f"None of the preferred formats {PREFERENCE_ORDER} found in article "
+            f"links. {[tag.get_text(strip=True) for tag in link_tags]}"
         )
 
-        article_tag, article_format = self._find_tag_with_best_format(link_tags)
+    if "href" not in article_tag.attrs:
+        article_tag = article_tag.find("a")
 
-        link_to_raw_file = article_tag["href"]
+    return cast("Tag", article_tag), ArticleFormat(article_format.lower())
 
-        return BaseArticle(
-            created_at=datetime.now(),
-            journal=raw_metadata.get("DC.Source", None)
-            or raw_metadata.get("citation_journal_title", ""),
-            url=self.article_url,
-            parsed=False,
-            raw_metadata=raw_metadata,
-            format=article_format,
-            url_to_raw_file=cast("str", link_to_raw_file),
-        )
+
+def scrape_article(
+    article_url: str,
+    article_content_pattern: re.Pattern = re.compile(
+        r"/article/view/[A-Za-z0-9-_.]+/[A-Za-z0-9-_.]+/?$"
+    ),
+) -> BaseArticle:
+    article_soup = soupify(article_url)
+    raw_metadata = _extract_metadata(article_soup)
+
+    link_tags = cast(
+        "list[Tag]",
+        article_soup.find_all("a", href=article_content_pattern),  # type: ignore
+    )
+
+    article_tag, article_format = _find_tag_with_best_format(link_tags)
+
+    link_to_raw_file = article_tag["href"]
+
+    return BaseArticle(
+        created_at=datetime.now(),
+        journal=raw_metadata.get("DC.Source", None)
+        or raw_metadata.get("citation_journal_title", ""),
+        url=article_url,
+        parsed=False,
+        raw_metadata=raw_metadata,
+        format=article_format,
+        url_to_raw_file=cast("str", link_to_raw_file),
+    )
--- src/ojs_scraper/scrape/issue.py
+++ src/ojs_scraper/scrape/issue.py
@@ -1,6 +1,5 @@
 import logging
 import re
-from collections.abc import Iterable
 from typing import cast
 
 from ojs_scraper.utils.soup import soupify
@@ -9,28 +8,17 @@ from ojs_scraper.utils.types import Issue, Link
 logger = logging.getLogger(__name__)
 
 
-class IssueScraper:
-    def __init__(
-        self, issue_url: str, issue_a_tag_for_article__parent: str | None = None
-    ) -> None:
-        self.issue_url = issue_url
-        self.issue: Issue = soupify(issue_url)
+def scrape_issue(
+    issue_url: str,
+    article_view_pattern: re.Pattern = re.compile(r"/article/view/[A-Za-z0-9-_.]+/?$"),
+) -> list[Link]:
+    issue: Issue = soupify(issue_url)
+    all_article_a_tags = issue.find_all("a", href=article_view_pattern)
 
-        self.issue_a_tag_for_article__parent = (
-            issue_a_tag_for_article__parent or "obj_article_summary"
-        )
-
-    def scrape_links_for_articles(self) -> Iterable[Link]:
-        article_tags = self.issue.select(f".{self.issue_a_tag_for_article__parent}")
-
-        article_view_pattern = r"/article/view/[A-Za-z0-9-_.]+/?$"
-
-        # Extraer los links.
-        for article_summary in article_tags:
-            link_tags = article_summary.find_all("a")
-
-            for link_tag in link_tags:
-                link = cast("str", link_tag.get("href", ""))  # type: ignore
-
-                if link != "" and re.search(article_view_pattern, link):
-                    yield link
+    return list(
+        {
+            cast("Link", a.get("href"))
+            for a in all_article_a_tags
+            if a.get("href") is not None
+        }
+    )
