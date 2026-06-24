de87795751a18e2d28753cc5c8b7a65fb7b1e096
Miguel González Duque <miguelgondu@gmail.com>
2026-06-24 12:08:25 -0500

    feat: modifies the logic of scraper to use functions and re instead of classes

--- src/ojs_scraper/scrape/archive.py
+++ src/ojs_scraper/scrape/archive.py
@@ -8,41 +8,36 @@ from ojs_scraper.utils.types import Link
 logger = logging.getLogger(__name__)
 
 
-class ArchiveScraper:
-    def __init__(
-        self,
-        base_url: str,
-    ):
-        self.base_url = base_url
-        self.issue_view_pattern = re.compile(r"/issue/view/[A-Za-z0-9-_.]+/?$")
+def scrape_archive(
+    base_url: str,
+    issue_view_pattern: re.Pattern = re.compile(r"/issue/view/[A-Za-z0-9-_.]+/?$"),
+    *,
+    max_counter: int = 500,
+) -> list[Link]:
+    """A list over the issue links."""
+    counter = 1
+    issue_pages = set()
 
-    def scrape_links_for_issues(self) -> list[Link]:
-        """An iterable over the issue links."""
-        counter = 1
-        issue_pages = set()
+    while counter < max_counter:
+        current_url = f"{base_url}/{counter}"
+        current_page_soup = soupify(current_url)
 
-        while counter < 500:
-            current_url = f"{self.base_url}/{counter}"
-            current_page_soup = soupify(current_url)
+        curent_issue_a_tags = current_page_soup.find_all("a", href=issue_view_pattern)
+        current_issue_links = {
+            cast("Link", link.get("href"))
+            for link in curent_issue_a_tags
+            if link.get("href")
+        }
 
-            curent_issue_a_tags = current_page_soup.find_all(
-                "a", href=self.issue_view_pattern
+        if issue_pages.union(current_issue_links) > issue_pages:
+            issue_pages = issue_pages.union(current_issue_links)
+            logger.info(
+                "Visited and added %d links from %s",
+                len(current_issue_links),
+                current_url,
             )
-            current_issue_links = {
-                cast("Link", link.get("href"))
-                for link in curent_issue_a_tags
-                if link.get("href")
-            }
+            counter += 1
+        else:
+            break
 
-            if issue_pages.union(current_issue_links) > issue_pages:
-                issue_pages = issue_pages.union(current_issue_links)
-                logger.info(
-                    "Visited and added %d links from %s",
-                    len(current_issue_links),
-                    current_url,
-                )
-                counter += 1
-            else:
-                break
-
-        return list(issue_pages)
+    return list(issue_pages)
