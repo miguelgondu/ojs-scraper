32c61b74adfa511c8072039159fa1436c4f53e16
Juan R. Loaiza <juanrloaiza@gmail.com>
2026-06-24 15:53:36 -0400

    tests: adapts tests for article scraper

--- tests/ojs_scraper/scrape/test_article.py
+++ tests/ojs_scraper/scrape/test_article.py
@@ -7,57 +7,42 @@ import pytest
 from bs4 import BeautifulSoup
 
 from ojs_scraper.models.article import ArticleFormat, BaseArticle
-from ojs_scraper.scrape.article import ArticleScraper
+from ojs_scraper.scrape.article import scrape_article
+
+DEFAULT_TEST_ARTICLE_URL = "test.url/journal/article/view/article123"
 
 
-def get_mock_scraper(
-    minimal_html: str, article_galley_class: str | None = None
-) -> ArticleScraper:
+def scrape_article_with_mock_soupify(minimal_html: str):
     with patch("ojs_scraper.scrape.article.soupify") as mock_soupify:
         mock_soupify.return_value = BeautifulSoup(minimal_html, features="lxml")
-
-        return ArticleScraper(
-            article_url="test.url", article_galley_class=article_galley_class
-        )
+        return scrape_article(DEFAULT_TEST_ARTICLE_URL)
 
 
-def test_scraper_with_default_article_galley_class() -> None:
+def test_scrape_article_select_only_tags_with_valid_href() -> None:
     minimal_html = """
-        <a class='obj_galley_link'>Correct Tag</a>
-        <a class='test_galley_class'>Invalid Tag</a>
+        <a href="/article/view/123/article123/">PDF</a>
+        <a href="" incorrect>PDF</a>
+        <a href="/wrong/pattern/" incorrect>PDF</a>
     """
-    scraper = get_mock_scraper(minimal_html, article_galley_class=None)
-    test_tag = scraper.article_soup.find(
-        attrs={"class": f"{scraper.article_galley_class}"}
-    )
-
-    assert test_tag.text == "Correct Tag"
+    result_article = scrape_article_with_mock_soupify(minimal_html)
+    assert result_article.format == ArticleFormat.PDF
 
 
-def test_scraper_with_custom_article_galley_class() -> None:
-    minimal_html = """
-        <a class='obj_galley_link'>PDF</a>
-        <a class='test_galley_class'>Test Tag</a>
-    """
-    scraper = get_mock_scraper(minimal_html, article_galley_class="test_galley_class")
-    test_tag = scraper.article_soup.find(
-        attrs={"class": f"{scraper.article_galley_class}"}
-    )
-
-    assert test_tag.text == "Test Tag"
-
-
-def test_extract_metadata() -> None:
-    minimal_html = """
+def test_scrape_article_correct_metadata() -> None:
+    minimal_html = f"""
+        <head>
         <meta name="DC.Source" content="Test Journal"/>
         <meta name="citation_journal_title" content="Test Journal - Fallback Meta Tag"/>
         <meta name="DC.Title" content="Test Article Title"/>
         <meta content="Meta Tag with no name"/>
+        </head>
+        <body>
+            <a href="{DEFAULT_TEST_ARTICLE_URL}/file123">PDF</a>
+        </body>
     """
-    scraper = get_mock_scraper(minimal_html)
-    metadata = scraper.extract_metadata()
+    result_article = scrape_article_with_mock_soupify(minimal_html)
 
-    assert metadata == {
+    assert result_article.raw_metadata == {
         "DC.Source": "Test Journal",
         "citation_journal_title": "Test Journal - Fallback Meta Tag",
         "DC.Title": "Test Article Title",
@@ -69,69 +54,43 @@ def test_extract_metadata() -> None:
     [
         (
             """
-        <a class="obj_galley_link pdf" href="Test Link">PDF</a>
-        <a class="obj_galley_link html" href="Test Link">HTML</a>
-        <a class="obj_galley_link xml" href="Test Link">XML</a>
+        <a href="/article/view/123/article123/">PDF</a>
+        <a href="/article/view/123/article456/">HTML</a>
+        <a href="/article/view/123/article789/">XML</a>
         """,
             ArticleFormat.XML,
         ),
         (
             """
-        <a class="obj_galley_link pdf" href="Test Link">PDF</a>
-        <a class="obj_galley_link html" href="Test Link">HTML</a>
+        <a href="/article/view/123/article123/">PDF</a>
+        <a href="/article/view/123/article456/">HTML</a>
         """,
             ArticleFormat.HTML,
         ),
         (
             """
-        <a class="obj_galley_link pdf" href="Test Link">PDF</a>
+        <a href="/article/view/123/article123/">PDF</a>
         """,
             ArticleFormat.PDF,
         ),
     ],
 )
-def test__find_tag_with_best_format_pick_expected_value(
+def test_scrape_article_with_best_format(
     minimal_html: str, expected_format: ArticleFormat
 ) -> None:
-    scraper = get_mock_scraper(minimal_html)
-    link_tags = scraper.article_soup.find_all(
-        attrs={"class": f"{scraper.article_galley_class}"}
-    )
-
-    best_tag = scraper._find_tag_with_best_format(link_tags)
-
-    assert best_tag[1] == expected_format
+    result_article = scrape_article_with_mock_soupify(minimal_html)
+    assert result_article.format == expected_format
 
 
-def test__find_tag_with_best_format_if_a_tag_is_contained_in_div() -> None:
-    minimal_html = """
-    '<div class="obj_galley_link">
-        <a href="TARGET A TAG">PDF</a>
-    </div>
-    """
-    scraper = get_mock_scraper(minimal_html)
-    link_tags = scraper.article_soup.find_all(
-        attrs={"class": f"{scraper.article_galley_class}"}
-    )
-
-    selected_tag, _ = scraper._find_tag_with_best_format(link_tags)
-
-    assert selected_tag.get("href") == "TARGET A TAG"
-
-
-def test__find_tag_with_best_format_raise_error_for_not_found() -> None:
-    minimal_html = '<a class="obj_galley_link" href="Test Link">Invalid Format</a>'
-    scraper = get_mock_scraper(minimal_html)
-    link_tags = scraper.article_soup.find_all(
-        attrs={"class": f"{scraper.article_galley_class}"}
-    )
-
+def test_scrape_article_raise_error_if_no_format_found() -> None:
+    minimal_html = '<a href="/article/view/123/article123/">Invalid Format</a>'
     with pytest.raises(ValueError):
-        scraper._find_tag_with_best_format(link_tags)
+        scrape_article_with_mock_soupify(minimal_html)
 
 
 def test_scrape_article_with_dc_source_metadata():
-    minimal_html = """
+    raw_file_url = f"{DEFAULT_TEST_ARTICLE_URL}/file123/"
+    minimal_html = f"""
     <html>
         <head>
             <meta name="DC.Source" content="Test Journal"/>
@@ -140,21 +99,21 @@ def test_scrape_article_with_dc_source_metadata():
             <meta content="Meta Tag with no name"/>
             </head>
         <body>
-            <a class="obj_galley_link pdf" href="Test Link">PDF</a>
+            <a href="{raw_file_url}">PDF</a>
         </body>
     </html>
     """
-    scraper = get_mock_scraper(minimal_html)
-    article = scraper.scrape_article()
-    assert isinstance(article, BaseArticle)
-    assert article.journal == "Test Journal"
-    assert article.url == "test.url"
-    assert article.format == ArticleFormat.PDF
-    assert article.url_to_raw_file == "Test Link"
+    result_article = scrape_article_with_mock_soupify(minimal_html)
+    assert isinstance(result_article, BaseArticle)
+    assert result_article.journal == "Test Journal"
+    assert result_article.url == DEFAULT_TEST_ARTICLE_URL
+    assert result_article.format == ArticleFormat.PDF
+    assert result_article.url_to_raw_file == raw_file_url
 
 
 def test_scrape_article_with_citation_journal_metadata():
-    minimal_html = """
+    raw_file_url = f"{DEFAULT_TEST_ARTICLE_URL}/file123/"
+    minimal_html = f"""
     <html>
         <head>
             <meta name="citation_journal_title" content="Test Journal - Fallback Meta Tag"/>
@@ -162,14 +121,14 @@ def test_scrape_article_with_citation_journal_metadata():
             <meta content="Meta Tag with no name"/>
             </head>
         <body>
-            <a class="obj_galley_link pdf" href="Test Link">PDF</a>
+            <a href="{raw_file_url}">PDF</a>
         </body>
     </html>
     """
-    scraper = get_mock_scraper(minimal_html)
-    article = scraper.scrape_article()
-    assert isinstance(article, BaseArticle)
-    assert article.journal == "Test Journal - Fallback Meta Tag"
-    assert article.url == "test.url"
-    assert article.format == ArticleFormat.PDF
-    assert article.url_to_raw_file == "Test Link"
+    result_article = scrape_article_with_mock_soupify(minimal_html)
+
+    assert isinstance(result_article, BaseArticle)
+    assert result_article.journal == "Test Journal - Fallback Meta Tag"
+    assert result_article.url == DEFAULT_TEST_ARTICLE_URL
+    assert result_article.format == ArticleFormat.PDF
+    assert result_article.url_to_raw_file == raw_file_url
